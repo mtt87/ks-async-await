cfdd6c7a2ae5dce5f775f41e900303362a0c2340
Mattia <mattia.asti@tesglobal.com>
2017-11-15 17:31:36 +0000

    promise test

--- 4.test.js
+++ 4.test.js
@@ -1,5 +1,33 @@
 const puppeteer = require('puppeteer');
 
+function getPageTitlePromise(url) {
+  return new Promise((resolve, reject) => {
+    let browser;
+    let page;
+    let pageTitle;
+    puppeteer.launch()
+      .then(b => {
+        browser = b;
+        return browser.newPage();
+      })
+      .then(p => {
+        page = p;
+        return page.goto(url)
+          .then(() => page.$('title'))
+          .then(titleEl => page.evaluate(body => body.innerHTML, TitleEl))
+          .then(pTitle => {
+            pageTitle = pTitle;
+          })
+          .then(() => browser.close())
+          .then(() => resolve(pageTitle))
+      })
+      .catch(err => {
+        console.log(err);
+        reject(err);
+      })
+  })
+}
+
 async function getPageTitle(url) {
   try {
     const browser = await puppeteer.launch();
@@ -14,6 +42,18 @@ async function getPageTitle(url) {
     return err;
   }
 }
+
+test('example.com page title should be "Example Domain"', async () => {
+  getPageTitle('http://www.example.com').then((pageTitle) => {
+    expect(pageTitle).toBe('Example Domain');
+  })
+});
+
+test('google.com page title should be "Google"', async () => {
+  getPageTitle('http://www.google.com').then((pageTitle) => {
+    expect(pageTitle).toBe('Google');
+  })
+});
 
 test('example.com page title should be "Example Domain"', async () => {
   expect(await getPageTitle('http://www.example.com')).toBe('Example Domain');
